617afa093e1705449060a42ff1a9a61a84448a07
papalino456 <sebastian.barrio.b@gmail.com>
2023-04-16 15:07:36 -0600

    main section commit

--- climber-app/src/components/mainSection.jsx
+++ climber-app/src/components/mainSection.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import user_circleIcon from '@heroicons/react/24/outline';
 
 export default function MainSection() {
   return(
     <div className='h-screen w-8/12'>
-        Main-Section
+      <div>
+        <h1 className='text-4xl font-bold ml-8 mt-3'>Dashboard</h1>
+        <user_circleIcon className='h-10 w-10 ml-8 mt-3'></user_circleIcon>
+        
+        </div>
     </div>
   );
 }
