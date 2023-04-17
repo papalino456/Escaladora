6c9b8b06f576832f66d85b6fcc43c2bcd74d7dcf
papalino456 <sebastian.barrio.b@gmail.com>
2023-04-16 18:58:02 -0600

    add routes to side bar and friends page section

---
+++ climber-app/src/components/FriendsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FriendsPage = () => {
+  return (
+    <div>
+      <h1>Friends Page</h1>
+      
+    </div>
+  );
+};
+
+export default FriendsPage;
--- climber-app/src/main.jsx
+++ climber-app/src/main.jsx
@@ -4,14 +4,21 @@ import './index.css'
 import SideBar from './components/sideBar'
 import MainSection from './components/mainSection'
 import GraphSection from './components/graphSection'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FriendsPage from './components/FriendsPage'
+import ProfileSection from './components/profileSection'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className="flex flex-row h-screen w-screen">
-      <SideBar />
-      <MainSection />
-      <GraphSection />
-    </div>
+    <Router>
+      <div className="flex flex-row h-screen w-screen">
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<> <MainSection /> <GraphSection /> </>} />
+          <Route path="/friends" element={<> <FriendsPage /> <ProfileSection /> </>} />
+        </Routes>
+      </div>
+    </Router>
   </React.StrictMode>,
 )
