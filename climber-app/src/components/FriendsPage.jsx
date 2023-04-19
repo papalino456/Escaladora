e3f4b49bfab8a77884bbba6337a850b2c9dbd7ee
¨Abraham-Trejo-Arvizu¨ <user.nameAbraham-Trejo-Arvizu>
2023-04-19 04:55:31 -0600

    amigos bar flex

--- climber-app/src/components/FriendsPage.jsx
+++ climber-app/src/components/FriendsPage.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const FriendsPage = () => {
   return (
-    <div>
-      <h1>Friends Page</h1>
-      
+    <div className='flex flex-auto items-center justify-center bg-red-300'>
+      <div className='flex bg-slate-500'> 
+        <h1>Friends Page</h1> 
+      </div>
+      <div class="flex items-center justify-center bg-purple-300" >
+        <img src='src\assets\profile.jpg' className='object-cover h-20 w-20 rounded-full ms-8'></img>
+      </div>
     </div>
   );
 };
