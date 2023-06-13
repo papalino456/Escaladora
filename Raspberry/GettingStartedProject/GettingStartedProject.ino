66c93ac3e2dbddb70b72d05e778559cd3337f7d3
papalino456 <sebastian.barrio.b@gmail.com>
2023-06-13 16:39:23 -0600

    Amplify heart rate signal + peak detection

--- Raspberry/GettingStartedProject/GettingStartedProject.ino
+++ Raspberry/GettingStartedProject/GettingStartedProject.ino
@@ -1,40 +1,21 @@
-
-/*  PulseSensor Starter Project and Signal Tester
- *  The Best Way to Get Started  With, or See the Raw Signal of, your PulseSensor.com™ & Arduino.
- *
- *  Here is a link to the tutorial
- *  https://pulsesensor.com/pages/code-and-guide
- *
- *  WATCH ME (Tutorial Video):
- *  https://www.youtube.com/watch?v=RbB8NSRa5X4
- *
- *
--------------------------------------------------------------
-1) This shows a live human Heartbeat Pulse.
-2) Live visualization in Arduino's Cool "Serial Plotter".
-3) Blink an LED on each Heartbeat.
-4) This is the direct Pulse Sensor's Signal.
-5) A great first-step in troubleshooting your circuit and connections.
-6) "Human-readable" code that is newbie friendly."
-
-*/
-
+#include <PeakDetection.h>
+#include "Filter.h"
 
 //  Variables
 int PulseSensorPurplePin = 0;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
 int LED = LED_BUILTIN;   //  The on-board Arduion LED
-#include "Filter.h"
-
 int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
 int Threshold = 580;       // Determine which Signal to "count as a beat", and which to ingore.
 
 ExponentialFilter<float> FilteredSignal(20,0);
 
+PeakDetection peakDetection;
+
 // The SetUp Function:
 void setup() {
   pinMode(LED,OUTPUT);         // pin that will blink to your heartbeat!
    Serial.begin(9600);         // Set's up Serial Communication at certain speed.
-
+  peakDetection.begin(30, 1.96, 0.5);
 }
 
 // The Main Loop Function
@@ -47,13 +28,18 @@ void loop() {
    FilteredSignal.Filter(Signal);
    float SmoothSignal = FilteredSignal.Current();
    long mapped = map(SmoothSignal,300.0,800.0,0.0,10.0);
-   long squared = pow(mapped,2);
+   long amplified = SmoothSignal*100 - 35000;
+   peakDetection.add(amplified);
+   int peak = (peakDetection.getPeak()*10000);
+
      Serial.print("smooth:");
     Serial.print(SmoothSignal);
-    Serial.print(",mapped:");
-    Serial.print(mapped);
-    Serial.print(",squared:");
-    Serial.println(squared);
+    Serial.print(",threshold:");
+    Serial.print(300);
+    Serial.print(",peak:");
+    Serial.print(peak);
+    Serial.print(",ampified:");
+    Serial.println(amplified);
 
    if(Signal > Threshold){                          // If the signal is above "550", then "turn-on" Arduino's on-Board LED.
      digitalWrite(LED,HIGH);
